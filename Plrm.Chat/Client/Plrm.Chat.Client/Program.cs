1582f17f919e2cc67881adc98fba5c4e3f5080ae
Boris Vashov <vashovway@gmail.com>
2021-10-26 22:10:56 +0300

    create client-server projects

---
+++ Plrm.Chat/Client/Plrm.Chat.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Plrm.Chat.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Plrm.Chat/Server/Plrm.Chat.Server.Gate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Plrm.Chat.Server.Gate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
